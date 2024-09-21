1debf8f072bbf0d697c872a9bfd3269b88a873f6
LAKSHMAN41 <ln9968977734@gmail.com>
2024-09-22 02:34:14 +0530

    Student and teacher tescases fixed

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,24 @@
--- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+
+-- SELECT TEACHER_ID, COUNT(*) AS ASSIGMENT_COUNT
+-- FROM ASSIGNMENTS
+-- WHERE TEACHER_ID IS NOT NULL
+-- AND STATE = 'GRADED'
+-- ORDER BY ASSIGMENT_COUNT DESC
+
+WITH GradedAssignments AS (
+    SELECT teacher_id, COUNT(*) AS graded_count
+    FROM assignments
+    WHERE state = 'GRADED'
+    GROUP BY teacher_id
+),
+MaxGradedTeacher AS (
+    SELECT teacher_id
+    FROM GradedAssignments
+    ORDER BY graded_count DESC
+    LIMIT 1
+)
+SELECT COUNT(*) AS grade_a_count
+FROM assignments
+WHERE teacher_id = (SELECT teacher_id FROM MaxGradedTeacher)
+AND state = 'GRADED'
+AND grade = 'A';
--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,5 @@
 -- Write query to get number of graded assignments for each student:
+SELECT ID, COUNT(*)
+FROM ASSIGNMENTS
+WHERE ID = 1
+AND STATE = 'GRADED'
